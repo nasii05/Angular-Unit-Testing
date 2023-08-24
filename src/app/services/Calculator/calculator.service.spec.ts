8a9a5c2b694e535435644d55d6e62abd2882ba58
nasii05 <iamnasih@gmail.com>
2023-08-24 09:49:05 +0530

    beforeEach function added 🙌

--- src/app/services/Calculator/calculator.service.spec.ts
+++ src/app/services/Calculator/calculator.service.spec.ts
@@ -2,20 +2,26 @@ import { LoggerService } from "../Logger/logger.service";
 import { CalculatorService } from "./calculator.service";
 
 describe('CalculatorService',()=>{
+  
+  let mockLoggerService:any;
+  let calculator:CalculatorService;
+
+  beforeEach(()=>{
+     mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
+     calculator = new CalculatorService(mockLoggerService);
+  })
+
   it('should add two numbers',()=>{
-    let loggerService = new LoggerService();
-    spyOn(loggerService, 'log')
-    const calculator = new CalculatorService(loggerService);
     let result = calculator.add(2,2);
     expect(result).toBe(4);
-    expect(loggerService.log).toHaveBeenCalledTimes(1);
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
+
   it('should subtract two numbers', ()=>{
-    let loggerService = new LoggerService();
-    spyOn(loggerService, 'log');
-    let calculator = new CalculatorService(loggerService);
     let result = calculator.subtract(2,2);
     expect(result).toBe(0);
-    expect(loggerService.log).toHaveBeenCalledTimes(1);
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   });
 });
+
+console.log(CalculatorService)
--- src/app/services/Logger/logger.service.ts
+++ src/app/services/Logger/logger.service.ts
@@ -6,6 +6,11 @@ import { Injectable } from '@angular/core';
 export class LoggerService {
   mesages:string[] = [];
 
+  constructor(){
+    debugger;
+    // logic
+  }
+
   log(message:string){
     debugger;
     this.mesages.push(message);
