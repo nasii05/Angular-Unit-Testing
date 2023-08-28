d987b0afc2f033de2d979c8ee79d6c54a1ab5d6e
nasii05 <iamnasih@gmail.com>
2023-08-28 11:11:19 +0530

    http client added

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/Post';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit{
+  posts:Post[] = [];
+
+  constructor(){}
+
+  ngOnInit(): void {
+    this.getPosts();
+  }
+  
+
+  getPosts(){
+    
+  }
+
+  delete(post){
+
+  }
 
 }
